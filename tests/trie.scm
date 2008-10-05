3ee25f35695b0d51035b22c63e6dafbc5a01531c
kana <nicht@s8.xrea.com>
2008-10-05 14:19:05 +0900

    Add more tests for vigorsh.trie, not passed yet

--- tests/trie.scm
+++ tests/trie.scm
@@ -33,7 +33,84 @@
 
 (test-section "(make-trie)")
 (define trie (make-trie))
-(test* "class-of" <trie> (class-of trie))
+(test* "class" <trie> (class-of trie))
+(test* "table" <hash-table> (class-of (slot-ref trie 'table)))
+
+
+(test-section "(trie-get1)")
+
+(define subtrie-f (trie-get1 trie #\f))
+(trie-put! trie "foo" 'foo)
+(test* "(trie-get1 trie #\\f)" <trie>
+       (class-of subtrie-f))
+(test* "... #\\o" <trie>
+       (class-of (trie-get1 subtrie-f #\o)))
+(test* "... #\\o" <trie>
+       (class-of (trie-get1 (trie-get1 subtrie-f #\o) #\o)))
+(test* "... #\\X" #f
+       (trie-get1 (trie-get1 (trie-get1 subtrie-f #\o) #\o) #\X))
+
+(trie-put! trie "bar" 'bar)
+(trie-put! trie "baz" 'baz)
+(define subtrie-b (trie-get1 trie #\b))
+(define subtrie-z (trie-get1 trie #\z))
+(test* "(trie-get1 trie #\\b)" <trie>
+       (class-of subtrie-b))
+(test* "... #\\a" <trie>
+       (class-of (trie-get1 subtrie-b #\a)))
+(test* "... #\\r" <trie>
+       (class-of (trie-get1 (trie-get1 subtrie-b #\a) #\r)))
+(test* "... #\\z" <trie>
+       (class-of (trie-get1 (trie-get1 subtrie-b #\a) #\z)))
+(test* "(trie-get1 trie #\\z)" #f
+       subtrie-z)
+
+
+(test-section "(trie-get)")
+(test* "(trie-get trie \"foo\")" 'foo (trie-get trie "foo"))
+(test* "(trie-get trie \"bar\")" 'bar (trie-get trie "bar"))
+(test* "(trie-get trie \"baz\")" 'baz (trie-get trie "baz"))
+(test* "(trie-get trie \"z\")" #f (trie-get trie "z"))
+(test* "(trie-get trie \"foox\")" #f (trie-get trie "foox"))
+
+
+(test-section "(trie-delete-at!)")
+(trie-put! trie "ba" 'XXX)
+(test* "(trie-get trie \"ba\")" 'XXX (trie-get trie "ba"))
+(test* "(trie-get trie \"bar\")" 'bar (trie-get trie "bar"))
+(test* "(trie-get trie \"baz\")" 'baz (trie-get trie "baz"))
+(trie-delete-at! trie "ba")
+(test* "(trie-get trie \"ba\")" #f (trie-get trie "ba"))
+(test* "(trie-get trie \"bar\")" 'bar (trie-get trie "bar"))
+(test* "(trie-get trie \"baz\")" 'baz (trie-get trie "baz"))
+(trie-delete-at! trie "baz")
+(test* "(trie-get trie \"ba\")" #f (trie-get trie "ba"))
+(test* "(trie-get trie \"bar\")" 'bar (trie-get trie "bar"))
+(test* "(trie-get trie \"baz\")" #f (trie-get trie "baz"))
+
+(test-section "(trie-delete-under!)")
+(trie-put! trie "foobar" 'foobar)
+(trie-put! trie "foobarbaz" 'foobarbaz)
+(test* "(trie-get trie \"foo\")" 'foo (trie-get trie "foo"))
+(test* "(trie-get trie \"foobar\")" 'foobar (trie-get trie "foobar"))
+(test* "(trie-get trie \"foobarbaz\")" 'foobarbaz (trie-get trie "foobarbaz"))
+(trie-delete-under! trie "foobarbaz")
+(test* "(trie-get trie \"foo\")" 'foo (trie-get trie "foo"))
+(test* "(trie-get trie \"foobar\")" 'foobar (trie-get trie "foobar"))
+(test* "(trie-get trie \"foobarbaz\")" #f (trie-get trie "foobarbaz"))
+(trie-delete-under! trie "foob")
+(test* "(trie-get trie \"foo\")" 'foo (trie-get trie "foo"))
+(test* "(trie-get trie \"foobar\")" #f (trie-get trie "foobar"))
+(test* "(trie-get trie \"foobarbaz\")" #f (trie-get trie "foobarbaz"))
+
+(test-section "(trie-delete-under!)")
+(trie-clear! trie)
+(test* "(trie-get trie \"ba\")" #f (trie-get trie "ba"))
+(test* "(trie-get trie \"bar\")" #f (trie-get trie "bar"))
+(test* "(trie-get trie \"baz\")" #f (trie-get trie "baz"))
+(test* "(trie-get trie \"foo\")" #f (trie-get trie "foo"))
+(test* "(trie-get trie \"foobar\")" #f (trie-get trie "foobar"))
+(test* "(trie-get trie \"foobarbaz\")" #f (trie-get trie "foobarbaz"))
 
 
 
